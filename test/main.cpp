8c1f1918fd02fcabdd3ee89e079b658b3e62fbc0
Junichi Sasaki <junichi200123@gmail.com>
2021-12-03 00:34:17 +0900

    test/ にmain.cppを追加

---
+++ test/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <map>
+#include <vector>
+
+int main(int argc, char **argv) {
+  /* vector */
+  {
+
+  }
+
+  /* map */
+  {}
+
+  /* stack */
+  {}
+
+  /* set (bonus) */
+  {}
+}

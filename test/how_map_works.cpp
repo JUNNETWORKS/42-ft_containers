b60f553ebaf5aea3527dbb018e27712ea919192f
Junichi Sasaki <junichi200123@gmail.com>
2021-12-18 15:29:08 +0900

    add: how_<container>_works.cpp to see how container works

---
+++ test/how_map_works.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <map>
+
+template <typename K, typename V>
+void showMap(std::map<K, V> &m) {
+  typename std::map<K, V>::const_iterator it = m.begin();
+  for (int i = 0; it != m.end(); ++it, ++i) {
+    std::cout << "map[" << it->first << "]: " << it->second << std::endl;
+  }
+}
+
+int main() {
+  {
+    std::map<int, int> m;
+    for (int i = 0; i < 10; ++i) {
+      m.insert(std::make_pair(i, i * i));
+    }
+    showMap(m);
+  }
+}

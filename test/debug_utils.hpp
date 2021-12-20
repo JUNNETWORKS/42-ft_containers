ce28462c85180f47388d4ed84aea611df398430c
Junichi Sasaki <junichi200123@gmail.com>
2021-12-20 14:48:45 +0900

    add: debug_utils

---
+++ test/debug_utils.hpp
@@ -0,0 +1,36 @@
+#ifndef DEBUG_UTILS_H_
+#define DEBUG_UTILS_H_
+
+template <typename T>
+void printVectorInfo(std::vector<T>& vec) {
+  std::cout << "STL Vector Information"
+            << "\tsize: " << vec.size() << "\n"
+            << "\tcap: " << vec.capacity() << std::endl;
+}
+
+template <typename T>
+void printVector(std::vector<T>& vec) {
+  std::cout << "STL Vector Data" << std::endl;
+  for (typename std::vector<T>::const_iterator it = vec.begin();
+       it != vec.end(); ++it) {
+    std::cout << "\t" << *it << std::endl;
+  }
+}
+
+template <typename T>
+void printVectorInfo(ft::vector<T>& vec) {
+  std::cout << "Ft Vector Information"
+            << "\tsize: " << vec.size() << "\n"
+            << "\tcap: " << vec.capacity() << std::endl;
+}
+
+template <typename T>
+void printVector(ft::vector<T>& vec) {
+  std::cout << "Ft Vector Data" << std::endl;
+  for (typename ft::vector<T>::const_iterator it = vec.begin(); it != vec.end();
+       ++it) {
+    std::cout << "\t" << *it << std::endl;
+  }
+}
+
+#endif

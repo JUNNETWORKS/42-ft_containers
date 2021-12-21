81f2969e2a6ea6849b3e1e0fc61a90d674d1e6ad
Junichi Sasaki <junichi200123@gmail.com>
2021-12-22 00:57:30 +0900

    add: test is_integral(), enable_if() and is_same()
    close #10

--- test/type_traits_test.cpp
+++ test/type_traits_test.cpp
@@ -76,3 +76,56 @@ TEST(RemoveVolatile, ConstTolatileT) {
   EXPECT_FALSE(
       std::is_volatile<ft::remove_volatile<const volatile int>::type>::value);
 }
+
+// is_integral
+TEST(IsIntegral, Integrals) {
+  EXPECT_TRUE(ft::is_integral<bool>::value);
+  EXPECT_TRUE(ft::is_integral<char>::value);
+  EXPECT_TRUE(ft::is_integral<wchar_t>::value);
+  EXPECT_TRUE(ft::is_integral<short>::value);
+  EXPECT_TRUE(ft::is_integral<int>::value);
+  EXPECT_TRUE(ft::is_integral<long>::value);
+  EXPECT_TRUE(ft::is_integral<long long>::value);
+}
+
+struct MyStruct {};
+
+TEST(IsIntegral, NotIntegrals) {
+  EXPECT_FALSE(ft::is_integral<void>::value);
+  EXPECT_FALSE(ft::is_integral<float>::value);
+  EXPECT_FALSE(ft::is_integral<double>::value);
+  EXPECT_FALSE(ft::is_integral<char*>::value);
+  EXPECT_FALSE(ft::is_integral<MyStruct>::value);
+}
+
+template <class T>
+bool return_true_if_integral(
+    T val,
+    typename ft::enable_if<std::is_integral<T>::value>::type* = nullptr) {
+  return true;
+}
+template <class T>
+bool return_true_if_integral(
+    T val,
+    typename ft::disable_if<std::is_integral<T>::value>::type* = nullptr) {
+  return false;
+}
+
+TEST(EnableIf, EnableIfTIsIntegral) {
+  EXPECT_TRUE(return_true_if_integral<int>(10));
+}
+
+TEST(DisableIf, DisableIfIfTIsIntegral) {
+  EXPECT_FALSE(return_true_if_integral<const char*>("abc"));
+}
+
+TEST(IsSame, TwoAreSame) {
+  // テンプレート引数のカンマが関数マクロの引数の区切りと判定されるのを防ぐ為に括弧で囲む
+  EXPECT_TRUE((ft::is_same<int, signed int>::value));
+  EXPECT_TRUE((ft::is_same<int, std::int32_t>::value));
+}
+
+TEST(IsSame, TowAreNotSame) {
+  EXPECT_TRUE((ft::is_same<int, unsigned int>::value));
+  EXPECT_FALSE((ft::is_same<char, int>::value));
+}

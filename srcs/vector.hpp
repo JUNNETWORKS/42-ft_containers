4d5fd19921f23125f83e9488780dd7f0695c6353
Junichi Sasaki <junichi200123@gmail.com>
2022-02-09 14:50:49 +0900

    fix: vector.operator=()

--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -59,22 +59,11 @@ class vector {
     operator=(x);
   }
 
-  const vector<T, Allocator> &operator=(const vector<T, Allocator> &x) {
-    if (this != &x) {
+  const vector<T, Allocator> &operator=(const vector<T, Allocator> &rhs) {
+    if (this != &rhs) {
       // 自分の配列を捨てる
-      for (size_type i = 0; size(); i++) {
-        allocator.destroy(start_ + i);
-      }
-      allocator.deallocate(start_, cap_);
-
-      // 新たに配列を作成し, データをxからコピー
-      cap_ = x.cap_;
-      start_ = allocator.allocate(cap_);
-      for (size_type i = 0; i < x.size(); i++) {
-        allocator.construct(start_ + i, x[i]);
-      }
-      finish_ = start_ + x.size();
-      end_of_storage_ = start_ + cap_;
+      erase(begin(), end());
+      assign(rhs.begin(), rhs.end());
     }
     return *this;
   }

4d165345f5bdedffc90e11eb7ede39f7439b996a
Junichi Sasaki <junichi200123@gmail.com>
2022-02-23 19:37:43 +0900

    fix compare_benchmark_result.sh. This can handle timer is equal to 0

--- compare_benchmark_result.sh
+++ compare_benchmark_result.sh
@@ -2,6 +2,8 @@ awk '
 BEGIN {
   std = -1;
   ft = -1;
+  has_read_std = 0
+  has_read_ft = 0
   RED = "\033[31m";
   RESET = "\033[0m";
   slow_func_idx = 0
@@ -12,29 +14,36 @@ BEGIN {
   sub(/^std::/, "", title);
   getline;
   getline;
-  if(std == -1){
+  if(has_read_std == 0){
     where = match($0, /[0-9]+/)
     end = index($0, "[ns]")
     std = substr($0, where, end - where)
+    has_read_std = 1
   }
 }
 /^ft::/{
   getline;
   getline;
-  if(ft == -1){
+  if(has_read_ft == 0){
     where = match($0, /[0-9]+/)
     end = index($0, "[ns]")
     ft = substr($0, where, end - where)
+    has_read_ft = 1
   }
 }
 
 {
-  if(std > 0 && ft > 0){
+  if(has_read_std && has_read_ft){
+    has_read_std = 0
+    has_read_ft = 0
+    if (ft <= 0 || std <= 0) {
+      printf("速すぎて測れません\n")
+    }
     times_slower = ft / std;
     printf("%s: ft: %d, std: %d, ratio: %f\n", title, ft, std, times_slower);
     std=-1;
     ft=-1;
-    if(times_slower >= 10) {
+    if (times_slower >= 10) {
       printf("%s%f倍遅い！\n%s", RED, times_slower, RESET)
       slow_funcs[slow_func_idx] = title
       slow_func_idx += 1

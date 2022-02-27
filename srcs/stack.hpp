88b2e84521433ae56b755ca889202c1bc35e0091
Junichi Sasaki <junichi200123@gmail.com>
2022-02-27 12:46:57 +0900

    add typedef of reference and const reference in stack

--- srcs/stack.hpp
+++ srcs/stack.hpp
@@ -17,9 +17,11 @@ the following operations:
 template <class T, class Container = vector<T> >
 class stack {
  public:
-  typedef T value_type;
   typedef Container container_type;
-  typedef std::size_t size_type;
+  typedef Container::value_type value_type;
+  typedef Container::size_type size_type;
+  typedef typename Container::reference reference;
+  typedef typename Container::const_reference const_reference;
 
  protected:
   Container c;
